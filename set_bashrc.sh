939ae8212ea4508b9dd899e885da7d83b54da5be
arisakubota <rabbit.hole.5555555@gmail.com>
2018-09-03 02:13:14 +0900

    change set_bashrc.sh

--- set_bashrc.sh
+++ set_bashrc.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 DIR="user_setting"
-#SCRIPT="bashrc.sh"
-SCRIPT="test.sh"
+SCRIPT="bashrc.sh"
 
-if [ ! -d ${DIR} ]; then
-	mkdir ${DIR}
+if [ ! -d ${HOME}/${DIR} ]; then
+	mkdir ${HOME}/${DIR}
 fi
 
-cd ${DIR} 
+cd ${HOME}/${DIR} 
 
 echo "#!/bin/bash" > ${SCRIPT} 
 echo "" >> ${SCRIPT} 
 
 echo "###color" >> ${SCRIPT} 
-echo "PS1=\"\[\033[36m\]\h\[\033[0m\]\[\033[33m\]\W\[\033[0m\]\[\033[36m\]\u\[\033[0m\] $\"" >> ${SCRIPT} 
+echo "PS1=\"\[\033[36m\]\u\[\033[33m\]@\[\033[36m\]\h\[\033[0m\]:\[\033[33m\]\W\[\033[0m\] $\"" >> ${SCRIPT} 
 echo "###COLOR definition" >> ${SCRIPT}
 echo "export LSCOLORS=hxfxcxdxbxegedabagacad" >> ${SCRIPT}
 echo "" >> ${SCRIPT} 
 
 echo "###shell from vim" >> ${SCRIPT}
-echo "[[ -n \"\$VIMRUNTIME\" ]] && PS1=\"(VIM) :\[\033[36m\]\h\[\033[0m\]\[\033[33m\]\W\[\033[0m\]\[\033[36m\]\u\[\033[0m\] $\"" >> ${SCRIPT} 
+echo "[[ -n \"\$VIMRUNTIME\" ]] && PS1=\"(VIM) :\[\033[36m\]\u\[\033[33m\]@\[\033[36m\]\h\[\033[0m\]:\[\033[33m\]\W\[\033[0m\] $\"" >> ${SCRIPT} 
 echo "" >> ${SCRIPT}
 
 echo "###alias" >> ${SCRIPT}
